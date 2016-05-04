60e59bed80846a0a377dd31d8820a83642ecc018
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-05-04 23:44:17 +0200

    Edited form and splited emails to save them as sepperated record in DB with same msg

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,18 +10,11 @@ class InvitationsController < ApplicationController
 
   def create
     @invitation = Invitation.new(invitation_params)
-
-    respond_to do |format|
-      if valid_email?(@invitation.email) 
-        @invitation.save
-        format.html { redirect_to root_path, notice: 'Invitation was successfully created.' }
-        format.json { render :index, status: :created, location: :index }
-      else
-        flash[:error] = 'Bad email!'
-        format.html { render :validation, notice: 'Something went wrong!' }
-        format.json { render json: @invitation.errors, status: :unprocessable_entity }
-      end
+    invitation_params[:email].split(', ').each do |email|
+      Invitation.create(email: email, :message => invitation_params[:message])
     end
+
+    redirect_to root_path
   end
 
   def testingajax
@@ -35,6 +28,7 @@ class InvitationsController < ApplicationController
 
   def valid_email?(email)
     @VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
     email.present? && (email =~ @VALID_EMAIL_REGEX)
   end
 end
